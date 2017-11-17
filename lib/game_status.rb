54f43fa47eee994f4272196c66247cdaeff28fe3
jordaninthewind <jordan.kline@gmail.com>
2017-11-17 23:29:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -52,8 +52,8 @@ def won?(board)
       return i
     elsif board[first] == "O" && board[second] == "O" && board[third] == "O"
       return i
-    else
-      return false
+    elsif !(board[first] == "X" && board[second] == "X" && board[third] == "X" && board[first] == "O" && board[second] == "O" && board[third] == "O"
+      return false)
     end
   end
 end
