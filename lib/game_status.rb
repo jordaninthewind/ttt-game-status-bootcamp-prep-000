88617e82fa9af8dde5978580d566f45a043b453f
jordaninthewind <jordan.kline@gmail.com>
2017-11-17 23:32:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,35 +14,6 @@ WIN_COMBINATIONS = [
   [2,4,6]  # R to L Diag
 ]
 
-# for each win_combination in WIN_COMBINATIONS
-
-#   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-#   # grab each index from the win_combination that composes a win.
-
-# def win? (board)
-#
-#   win_index_1 = WIN_COMBINATIONS[0]
-#   win_index_2 = WIN_COMBINATIONS[1]
-#   win_index_3 = WIN_COMBINATIONS[2]
-#   win_index_4 = WIN_COMBINATIONS[3]
-#   win_index_5 = WIN_COMBINATIONS[4]
-#   win_index_6 = WIN_COMBINATIONS[5]
-#   win_index_7 = WIN_COMBINATIONS[6]
-#   win_index_8 = WIN_COMBINATIONS[7]
-#   win_index_9 = WIN_COMBINATIONS[8]
-#
-#   position_1 = board[win_index_1] # load the value of the board at win_index_1
-#   position_2 = board[win_index_2] # load the value of the board at win_index_2
-#   position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-
-#   if position_1 == "X" && position_2 == "X" && position_3 == "X"
-#     return win_combination # return the win_combination indexes that won.
-#   else
-#     false
-#   end
-# end
-
 def won?(board)
   WIN_COMBINATIONS.each do |i|
     first = i[0]
