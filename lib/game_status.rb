94b75874eb98424dc38805122d46037b634ba9e8
jordaninthewind <jordan.kline@gmail.com>
2017-11-17 22:58:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,13 +3,24 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+WIN_COMBINATIONS = [
+  [0,1,2], # Top Horizontal
+  [3,4,5], # Middle Horizontal
+  [6,7,8], # Bottom Horizontal
+  [0,3,6], # Left Vertical
+  [1,4,7], # Middle Vertical
+  [2,5,8], # Right Vertical
+  [0,4,8], # L to R Diag
+  [2,4,6]  # R to L Diag
+]
+
 # for each win_combination in WIN_COMBINATIONS
 
 #   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
 #   # grab each index from the win_combination that composes a win.
 
 # def win? (board)
-# 
+#
 #   win_index_1 = WIN_COMBINATIONS[0]
 #   win_index_2 = WIN_COMBINATIONS[1]
 #   win_index_3 = WIN_COMBINATIONS[2]
@@ -19,12 +30,12 @@ end
 #   win_index_7 = WIN_COMBINATIONS[6]
 #   win_index_8 = WIN_COMBINATIONS[7]
 #   win_index_9 = WIN_COMBINATIONS[8]
-#   
+#
 #   position_1 = board[win_index_1] # load the value of the board at win_index_1
 #   position_2 = board[win_index_2] # load the value of the board at win_index_2
 #   position_3 = board[win_index_3] # load the value of the board at win_index_3
 
-  
+
 #   if position_1 == "X" && position_2 == "X" && position_3 == "X"
 #     return win_combination # return the win_combination indexes that won.
 #   else
@@ -33,17 +44,6 @@ end
 # end
 
 def won?(board)
-
-  WIN_COMBINATIONS = [
-    [0,1,2], # Top Horizontal
-    [3,4,5], # Middle Horizontal
-    [6,7,8], # Bottom Horizontal
-    [0,3,6], # Left Vertical
-    [1,4,7], # Middle Vertical
-    [2,5,8], # Right Vertical
-    [0,4,8], # L to R Diag
-    [2,4,6]  # R to L Diag
-  ]
 
   WIN_COMBINATIONS.each do |win|
     win.each do |i|
@@ -74,4 +74,3 @@ def winner()
 end
 
 # Define your WIN_COMBINATIONS constant
-
